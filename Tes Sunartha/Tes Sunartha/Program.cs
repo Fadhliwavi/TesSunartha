f55f88c9d83b4a6d6c5f903807d6a0846092055b
fadhli <fadhli.wavi123@gmail.com>
2022-09-13 20:44:12 +0700

    Test Sunartha_Fadhli Wavi

---
+++ Tes Sunartha/Tes Sunartha/Program.cs
@@ -0,0 +1,70 @@
+﻿using TesSunartha;
+
+using System;
+using System.ComponentModel;
+
+namespace TesSunartha
+{
+    class Program
+    {
+         static void Main(string[] args)
+        {
+            Console.Clear();
+
+            listMobil listMobil = new listMobil();
+            listMobil.No1();
+            listMobil.No2();
+        }
+
+
+        public static void PrintListMobil()
+        {
+            listMobil listMobil = new listMobil();
+            listMobil.IdRegistrasi = 0001;
+            listMobil.Tipe = "Sedan";
+            listMobil.Merk = "Toyota";
+            listMobil.Varian = "FT86";
+            Console.WriteLine(listMobil.IdRegistrasi);
+            Console.WriteLine(listMobil.Tipe);
+            Console.WriteLine(listMobil.Merk);
+            Console.WriteLine(listMobil.Varian);
+            listMobil.PrintListMobil();
+
+            listMobil listMobil1 = new listMobil();
+            listMobil.IdRegistrasi = 0002;
+            listMobil.Tipe = "Suv";
+            listMobil.Merk = "Toyota";
+            listMobil.Varian = "Rav$";
+            Console.WriteLine(listMobil.IdRegistrasi);
+            Console.WriteLine(listMobil.Tipe);
+            Console.WriteLine(listMobil.Merk);
+            Console.WriteLine(listMobil.Varian);
+            listMobil.PrintListMobil();
+
+            listMobil listMobil2 = new listMobil();
+            listMobil.IdRegistrasi = 0001;
+            listMobil.Tipe = "Sedan";
+            listMobil.Merk = "Honda";
+            listMobil.Varian = "Accord";
+            Console.WriteLine(listMobil.IdRegistrasi);
+            Console.WriteLine(listMobil.Tipe);
+            Console.WriteLine(listMobil.Merk);
+            Console.WriteLine(listMobil.Varian);
+            listMobil.PrintListMobil();
+
+            listMobil listMobil3 = new listMobil();
+            listMobil.IdRegistrasi = 0001;
+            listMobil.Tipe = "Suv";
+            listMobil.Merk = "Honda";
+            listMobil.Varian = "FT86";
+            Console.WriteLine(listMobil.IdRegistrasi);
+            Console.WriteLine(listMobil.Tipe);
+            Console.WriteLine(listMobil.Merk);
+            Console.WriteLine(listMobil.Varian);
+            listMobil.PrintListMobil();
+
+
+        }
+
+    }
+}
---
+++ Tes Sunartha/Tes Sunartha/listMobil.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TesSunartha
+{
+
+
+    /* Pertanyaan:
+        1. Tampilkan data pertama yang memiliki merk "Honda"
+        2. Tampilkan data terakhir yang memiliki merk "Honda" dan bertipe "Sedan"
+        3. Tampilkan data pertama yang memiliki merk "Honda" dan punya varian "City"
+        4. Tampilkan data default yang memiliki merk "Toyota"
+        5. Tampilkan 3 data apa saja
+        Format tampilan : IDRegistrasi, Merk, Varian*/
+    public class listMobil
+    {
+        public int IdRegistrasi { get; set; }
+        public string Tipe { get; set; }
+        public string Merk { get; set; }
+        public string Varian { get; set; }
+
+        public listMobil(int idRegistrasi, string tipe, string merk, string varian)
+        {
+            this.IdRegistrasi = idRegistrasi;
+            this.Tipe = tipe;
+            this.Merk = merk;
+            this.Varian = varian;
+        }
+
+        public listMobil()
+        {
+        }
+
+        public void PrintListMobil()
+        {
+            Console.WriteLine("\n\n\n---------------------------\n" + "Tipe\t\t\t: {0}", Tipe);
+            Console.WriteLine("Merk\t\t\t: {0}", Merk);
+            Console.WriteLine("Varian\t\t\t: {0}", Varian);
+        }
+
+
+
+
+
+
+        /*Ada 2 anak sedang bermain angka bersahutan, sebut saja anak A dan anak B, mereka 
+        saling bersahutan angka mulai dari 1 hingga 500. dimulai dari anak A berteriak "1", 
+        dilanjutkan anak B berteriak "2", kembali ke anak A melanjutkan teriak "3" dan 
+        seterusnya. untuk membuat permainan lebih menarik salah satu dari mereka 
+        mengusulkan untuk meneriakkan kata tertentu ketika mendapat giliran angka dengan 
+        kategori yang mereka tentukan.
+        1. "tik" jika angka merupakan kelipatan 3
+        2. "tek" jika angka merupakan kelipatan 5*/
+
+        public void No1()
+        {
+            int i, n, y;
+            Console.WriteLine("Masukan Angka : ");
+            n = Convert.ToInt32(Console.ReadLine());
+            y = 1;
+            i = 1;
+
+            while (i <= n)
+            {
+                if(y % 3 == 0)
+                {
+                    Console.Write("tik");
+                }
+                else if( y % 5 == 0)
+                {
+                    Console.Write("tek");
+                }
+                else
+                {
+                    Console.Write("{0}", y);
+                }
+                i++;
+                 
+            }
+        }
+
+        public void No2()
+        {
+            int i, n, y;
+            Console.WriteLine("Masukan Angka : ");
+            n = Convert.ToInt32(Console.ReadLine());
+            y = 1;
+            i = 1;
+
+            while (i <= n)
+            {
+                if (y % 3 == 0)
+                {
+                    Console.Write("tik");
+                }
+                else if (y % 5 == 0)
+                {
+                    Console.Write("tek");
+                }
+                else if(y % 7 == 0)
+                {
+                    Console.Write("tok");
+                }
+                else
+                {
+                    Console.Write("{0}", y);
+                }
+                i++;
+
+            }
+        }
+    }
+
+}
